d23cb2d1b9743ed9786a29fd2b8b990f21830968
gabrielrowan <gabrielrowan82@gmail.com>
2025-05-01 12:22:25 +0100

    navbar done

---
+++ ui/contributions-frontend/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import "../styles/Navbar.css"
+
+function NavBar() {
+    return (
+        <div className="navbar">
+            <h2 className="navbar-heading">Contributions</h2>
+        </div>
+    )
+
+}
+
+export default NavBar
